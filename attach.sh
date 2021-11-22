daaebcf0a28c4ba9b0d1221e41c87af133650256
test <test@test.test>
2021-11-22 17:59:30 +0800

    init

--- attach.sh
+++ attach.sh
@@ -2,8 +2,9 @@
 
 #docker pull ethereum/client-go:v1.9.24
 
-sudo docker exec -it mainnet-eth  /bin/sh
+#sudo docker exec -it mainnet-eth  /bin/sh
 #sudo docker attach 76e980019fe8
 #cd /root/.ethereum
-#geth --datadir=. attach ipc:./geth.ipc console
+#./geth_linux --datadir=node attach ipc:./geth.ipc console
+./geth_linux attach http://127.0.0.1:28545
 
